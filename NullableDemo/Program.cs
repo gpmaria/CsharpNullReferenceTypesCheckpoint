a1a6c3b761283059b7961525c09e412b3de43201
Mariya Petrova <mariya.petrova@autodesk.com>
2020-02-26 10:21:45 -0500

    Add project files.

---
+++ NullableDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NullableDemo
+{
+    public class Employee
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? PayRate { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee = new Employee
+            {
+                FirstName = "John"
+            };
+
+            var namelength = employee.FirstName.Length + employee.LastName?.Length;
+            Console.WriteLine($"The employee is called {employee.FirstName} { employee.LastName}");
+        }
+            //Console.WriteLine("Hello World!");
+        
+    }
+}
